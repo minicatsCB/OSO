2a0c4b7d99030ac549e3cee9a932d1fc93a5d20c
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2023-06-14 19:16:33 +0200

    Draw basic board

--- oso/src/components/board.tsx
+++ oso/src/components/board.tsx
@@ -1,5 +1,40 @@
-export default function Board() {
+import './Board.css';
+
+function getBoardStyle(numRows: number, numCols: number) {
+    return {
+        width: '300px',
+        height: '300px',
+        display: 'grid',
+        gridTemplateRows: `repeat(${numRows}, 1fr)`,
+        gridTemplateColumns: `repeat(${numCols}, 1fr)`,
+        gridGap: 2
+    };
+}
+
+function generateGrid(rows: number, cols: number) {
+    return new Array(rows).fill(0).map(() => new Array(cols).fill(0));
+}
+
+function Cell({text}: {[key: string]: string}) {
     return (
-        <p>Board works</p>
+        <button type="button" className="cell">{text}</button>
+    );
+}
+
+const ROWS_NUM :number = 3;
+const COLS_NUM :number = 3;
+
+export default function Board() {
+    const grid = generateGrid(ROWS_NUM, COLS_NUM);
+    const gridStyles = getBoardStyle(ROWS_NUM, COLS_NUM);
+
+    return (
+        <div className="container" style={gridStyles}>
+            {grid.map((row, rowIdx) =>
+                row.map((col, colIdx) => (
+                    <Cell key={`${rowIdx}-${colIdx}`} text={`${rowIdx}-${colIdx}`} />
+                ))
+            )}
+        </div>
     );
 }
