8fb22448e196a12693c040259e691eaba0f8f2fa
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2023-06-15 11:04:08 +0200

    Replace inline styles with styled component

--- oso/src/components/board.tsx
+++ oso/src/components/board.tsx
@@ -1,15 +1,17 @@
 import styles from './Board.module.css';
+import styled from 'styled-components'
 
-function getBoardStyle(numRows: number, numCols: number) {
-    return {
-        width: '300px',
-        height: '300px',
-        display: 'grid',
-        gridTemplateRows: `repeat(${numRows}, 1fr)`,
-        gridTemplateColumns: `repeat(${numCols}, 1fr)`,
-        gridGap: 2
-    };
-}
+const ROWS_NUM :number = 3;
+const COLS_NUM :number = 3;
+
+const Grid = styled.div`
+    width: 300px;
+    height: 300px;
+    display: grid;
+    grid-template-rows: repeat(${ROWS_NUM}, 1fr);
+    grid-template-columns: repeat(${COLS_NUM}, 1fr);
+    gridGap: 2;
+`;
 
 function generateGrid(rows: number, cols: number) {
     return new Array(rows).fill(0).map(() => new Array(cols).fill(0));
@@ -21,20 +23,16 @@ function Cell({text}: {[key: string]: string}) {
     );
 }
 
-const ROWS_NUM :number = 3;
-const COLS_NUM :number = 3;
-
 export default function Board() {
     const grid = generateGrid(ROWS_NUM, COLS_NUM);
-    const gridStyles = getBoardStyle(ROWS_NUM, COLS_NUM);
 
     return (
-        <div className="container" style={gridStyles}>
+        <Grid className="container">
             {grid.map((row, rowIdx) =>
                 row.map((col, colIdx) => (
                     <Cell key={`${rowIdx}-${colIdx}`} text={`${rowIdx}-${colIdx}`} />
                 ))
             )}
-        </div>
+        </Grid>
     );
 }
