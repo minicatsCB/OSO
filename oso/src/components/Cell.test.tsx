0406446df9971661a905abd076f9968d9cabb3d9
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2023-06-25 18:24:27 +0200

    Lift up context value

--- oso/src/components/Cell.test.tsx
+++ oso/src/components/Cell.test.tsx
@@ -2,11 +2,12 @@ import { render, screen } from '@testing-library/react';
 import Cell from './Cell';
 import { GameContext } from './Game';
 
+const contextValue: Array<string> = ['X', 'O', 'X', '', 'X', '', 'O', '', 'O'];
+
 test('shows empty cell if it has not been clicked yet', () => {
     const onClick = jest.fn();
-    const contextValue: Array<string> = ['X', 'O', 'X', '', 'X', '', 'O', '', 'O'];
-
     const cellIdx: number = 3;
+
     render(
         <GameContext.Provider value={contextValue}>
             <Cell key="1-0" index={cellIdx} onClick={() => onClick(cellIdx)}></Cell>
@@ -19,9 +20,8 @@ test('shows empty cell if it has not been clicked yet', () => {
 
 test('shows correct token for player X', () => {
     const onClick = jest.fn();
-    const contextValue: Array<string> = ['X', 'O', 'X', '', 'X', '', 'O', '', 'O'];
-
     const cellIdx: number = 2;
+
     render(
         <GameContext.Provider value={contextValue}>
             <Cell key="0-2" index={cellIdx} onClick={() => onClick(cellIdx)}></Cell>
@@ -34,9 +34,8 @@ test('shows correct token for player X', () => {
 
 test('shows correct token for player O', () => {
     const onClick = jest.fn();
-    const contextValue: Array<string> = ['X', 'O', 'X', '', 'X', '', 'O', '', 'O'];
-
     const cellIdx: number = 6;
+    
     render(
         <GameContext.Provider value={contextValue}>
             <Cell key="2-0" index={cellIdx} onClick={() => onClick(cellIdx)}></Cell>
