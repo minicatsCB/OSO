af8e69b545fb49d6b7f8d0f2bda8190ce0a8fd0b
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2023-06-25 18:14:22 +0200

    Add DOM tests

---
+++ oso/src/components/Cell.test.tsx
@@ -0,0 +1,48 @@
+import { render, screen } from '@testing-library/react';
+import Cell from './Cell';
+import { GameContext } from './Game';
+
+test('shows empty cell if it has not been clicked yet', () => {
+    const onClick = jest.fn();
+    const contextValue: Array<string> = ['X', 'O', 'X', '', 'X', '', 'O', '', 'O'];
+
+    const cellIdx: number = 3;
+    render(
+        <GameContext.Provider value={contextValue}>
+            <Cell key="1-0" index={cellIdx} onClick={() => onClick(cellIdx)}></Cell>
+        </GameContext.Provider>
+    );
+
+    const clickableCell = screen.getByRole('button');
+    expect(clickableCell).toHaveTextContent(contextValue[cellIdx]);
+});
+
+test('shows correct token for player X', () => {
+    const onClick = jest.fn();
+    const contextValue: Array<string> = ['X', 'O', 'X', '', 'X', '', 'O', '', 'O'];
+
+    const cellIdx: number = 2;
+    render(
+        <GameContext.Provider value={contextValue}>
+            <Cell key="0-2" index={cellIdx} onClick={() => onClick(cellIdx)}></Cell>
+        </GameContext.Provider>
+    );
+
+    const clickableCell = screen.getByRole('button');
+    expect(clickableCell).toHaveTextContent(contextValue[cellIdx]);
+});
+
+test('shows correct token for player O', () => {
+    const onClick = jest.fn();
+    const contextValue: Array<string> = ['X', 'O', 'X', '', 'X', '', 'O', '', 'O'];
+
+    const cellIdx: number = 6;
+    render(
+        <GameContext.Provider value={contextValue}>
+            <Cell key="2-0" index={cellIdx} onClick={() => onClick(cellIdx)}></Cell>
+        </GameContext.Provider>
+    );
+
+    const clickableCell = screen.getByRole('button');
+    expect(clickableCell).toHaveTextContent(contextValue[cellIdx]);
+});
