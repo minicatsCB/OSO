86a18985bad6848c050301a65c3a37067f7d9803
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2023-06-28 19:27:37 +0200

    Allow optional property access

--- oso/src/components/Cell.tsx
+++ oso/src/components/Cell.tsx
@@ -9,7 +9,7 @@ const Button = styled.button`
 export default function Cell({className, index, onClick}: any) {
     const value = useContext(GameContext);
 
-    const text = value.at(index);
+    const text = value?.at(index);
 
     return (
         <Button type="button" className={className} onClick={onClick}>{text}</Button>
