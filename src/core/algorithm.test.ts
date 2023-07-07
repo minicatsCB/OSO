f2fa1c6aac8e8d76cf93c9ff492a4873f84aea4d
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2023-07-07 10:21:24 +0200

    Add tests

--- src/core/algorithm.test.ts
+++ src/core/algorithm.test.ts
@@ -1,4 +1,5 @@
 import calculateWinner from "./algorithm";
+import { markIsValid } from "./algorithm";
 
 test('X wins', () => {
     const cells: Array<string> = ['X', 'O', 'X', '', 'X', '', 'X', 'O', 'O'];
@@ -13,4 +14,24 @@ test('O wins', () => {
 test('Draw', () => {
     const cells: Array<string> = ['X', 'O', 'X', '', 'X', '', 'O', 'X', 'O'];
     expect(calculateWinner(cells)).toBe('');
+});
+
+test('marked word is valid', () => {
+    const mark: Array<string> = ['O', 'S', 'O'];
+    expect(markIsValid(mark)).toBe(true);
+});
+
+test('marked word is invalid', () => {
+    const invalidMarks: Array<Array<string>> = [
+        ['O', 'O', 'O'],
+        ['O', 'O', 'S'],
+        ['O', 'S', 'S'],
+        ['S', 'S', 'S'],
+        ['S', 'S', 'O'],
+        ['S', 'O', 'O'],
+        ['S', 'O', 'S']
+    ];
+    for (const mark of invalidMarks) {
+        expect(markIsValid(mark)).toBe(false);   
+    }
 });
