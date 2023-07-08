3a754d6c32a9a9393317403d78695234656a6c5c
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2023-07-08 19:38:51 +0200

    Add token values

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -3,3 +3,6 @@ export const COLS : number = 3;
 
 export const FIRST_PLAYER : string = 'X';
 export const SECOND_PLAYER : string = 'O';
+
+export const O_TOKEN : string = 'O';
+export const S_TOKEN: string = 'S';
