a1580dc66e2ac14f3c5c964c2f1a3f8ff1a74513
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2023-08-01 16:50:19 +0200

    Update tests

--- src/components/Cell.test.tsx
+++ src/components/Cell.test.tsx
@@ -2,46 +2,21 @@ import { render, screen } from '@testing-library/react';
 import Cell from './Cell';
 import GameContext from '../core/gameContext';
 
-const contextValue: Array<string> = ['X', 'O', 'X', '', 'X', '', 'O', '', 'O'];
+const contextValue: Array<string> = ['O', '', '', '', 'S', '', '', '', ''];
 
-test('shows empty cell if it has not been clicked yet', () => {
+test('shows correct content', () => {
     const onClick = jest.fn();
-    const cellIdx: number = 3;
 
     render(
         <GameContext.Provider value={contextValue}>
-            <Cell key="1-0" index={cellIdx} onClick={() => onClick(cellIdx)}></Cell>
+            <Cell key="0-0" index={0} onClick={() => onClick(0, 1)}></Cell>
+            <Cell key="0-2" index={2} onClick={() => onClick(2, 0)}></Cell>
+            <Cell key="1-1" index={4} onClick={() => onClick(4, 2)}></Cell>
         </GameContext.Provider>
     );
 
-    const clickableCell = screen.getByRole('button');
-    expect(clickableCell).toHaveTextContent(contextValue[cellIdx]);
-});
-
-test('shows correct token for player X', () => {
-    const onClick = jest.fn();
-    const cellIdx: number = 2;
-
-    render(
-        <GameContext.Provider value={contextValue}>
-            <Cell key="0-2" index={cellIdx} onClick={() => onClick(cellIdx)}></Cell>
-        </GameContext.Provider>
-    );
-
-    const clickableCell = screen.getByRole('button');
-    expect(clickableCell).toHaveTextContent(contextValue[cellIdx]);
-});
-
-test('shows correct token for player O', () => {
-    const onClick = jest.fn();
-    const cellIdx: number = 6;
-    
-    render(
-        <GameContext.Provider value={contextValue}>
-            <Cell key="2-0" index={cellIdx} onClick={() => onClick(cellIdx)}></Cell>
-        </GameContext.Provider>
-    );
-
-    const clickableCell = screen.getByRole('button');
-    expect(clickableCell).toHaveTextContent(contextValue[cellIdx]);
+    const clickableCells = screen.getAllByRole('button');
+    expect(clickableCells[0]).toHaveTextContent(contextValue[0]);
+    expect(clickableCells[1]).toHaveTextContent(contextValue[2]);
+    expect(clickableCells[2]).toHaveTextContent(contextValue[4]);
 });
