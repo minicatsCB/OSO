48048f45aeb48caaa0166a2f3df88e2d52f7d56d
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2023-10-28 20:18:07 +0200

    Remove tests that are not responsability of Board

--- src/components/Board.test.tsx
+++ src/components/Board.test.tsx
@@ -1,7 +1,5 @@
-import { render, screen, waitFor, within } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
 import Board from './Board';
-import { act } from 'react-dom/test-utils';
-import userEvent from '@testing-library/user-event';
 
 test('renders 1x1 board correctly', () => {
     const ROWS: number = 1;
@@ -38,35 +36,3 @@ test('renders 5x3 board correctly', () => {
     const cells: Array<HTMLElement> = within(board).getAllByRole('button');
     expect(cells).toHaveLength(ROWS * COLS);
 });
-
-
-test('clicking cell once calls handler correctly', async () => {
-    const ROWS: number = 3;
-    const COLS: number = 3;
-    const handlePlay = jest.fn();
-
-    render(<Board rows={ROWS} cols={COLS} onPlay={handlePlay} />);
-
-    const board: HTMLElement = screen.getByTestId('board');
-    const cells: Array<HTMLElement> = within(board).getAllByRole('button');
-    expect(cells).toHaveLength(ROWS * COLS);
-
-    act(() => userEvent.click(cells[0]))
-    await waitFor(() => expect(handlePlay).toHaveBeenCalledWith(0, 1));
-});
-
-test('clicking cell twice calls handler correctly', async () => {
-    const ROWS: number = 3;
-    const COLS: number = 3;
-    const handlePlay = jest.fn();
-
-    render(<Board rows={ROWS} cols={COLS} onPlay={handlePlay} />);
-
-    const board: HTMLElement = screen.getByTestId('board');
-    const cells: Array<HTMLElement> = within(board).getAllByRole('button');
-    expect(cells).toHaveLength(ROWS * COLS);
-
-    act(() => userEvent.dblClick(cells[0]))
-    await waitFor(() => expect(handlePlay).toHaveBeenCalledWith(0, 2));
-});
-
