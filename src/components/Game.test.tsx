34a65a9c4c4a2f872eb380d53ef74e13f43cfa25
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2024-06-08 14:25:26 +0200

    test(game): same OSO word can not be marked twice

--- src/components/Game.test.tsx
+++ src/components/Game.test.tsx
@@ -171,3 +171,45 @@ test('if mark button is pressed, turn button does not work', async () => {
     act(() => userEvent.click(turnBtn));
     await screen.findByText(`It's Bob's turn`);
 })
+
+test('same OSO word can not be marked twice', async () => {
+    render(<Game />)
+
+    const board: HTMLElement = screen.getByTestId('board');
+    const cells: Array<HTMLElement> = within(board).getAllByRole('button');
+    const turnBtn: HTMLElement = screen.getByTestId('turn-btn');
+    const markBtn: HTMLElement = screen.getByTestId('mark-btn');
+    const endGameBtn: HTMLElement = screen.getByTestId('end-game-btn');
+
+    act(() => userEvent.click(cells[0]));
+    jest.advanceTimersByTime(SECOND_CLICK_WAIT_TIME);
+    await waitFor(() => expect(cells[0]).toHaveTextContent(O_TOKEN));
+    act(() => userEvent.click(turnBtn));
+    await screen.findByText(`It's Bob's turn`);
+
+    act(() => userEvent.dblClick(cells[1]));
+    await waitFor(() => expect(cells[1]).toHaveTextContent(S_TOKEN));
+    act(() => userEvent.click(turnBtn));
+    await screen.findByText(`It's Alice's turn`);
+
+    act(() => userEvent.click(cells[2]));
+    jest.advanceTimersByTime(SECOND_CLICK_WAIT_TIME);
+    await waitFor(() => expect(cells[2]).toHaveTextContent(O_TOKEN));
+    act(() => userEvent.click(markBtn));
+    expect(turnBtn).not.toBeEnabled();
+    expect(markBtn).toBeEnabled();
+    act(() => userEvent.click(cells[0]));
+    act(() => userEvent.click(cells[1]));
+    act(() => userEvent.click(cells[2]));
+    await screen.findByText('Alice: 1');
+
+    act(() => userEvent.click(cells[0]));
+    act(() => userEvent.click(cells[1]));
+    act(() => userEvent.click(cells[2]));
+    await screen.findByText('Alice: 1');
+
+    act(() => userEvent.click(cells[2]));
+    act(() => userEvent.click(cells[1]));
+    act(() => userEvent.click(cells[0]));
+    await screen.findByText('Alice: 1');
+});
