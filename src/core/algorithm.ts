9a694aaeb4c1ea19868e7c447fd25fb27a62c9a0
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2024-12-08 17:42:39 +0100

    chore: add JSDoc comments

--- src/core/algorithm.ts
+++ src/core/algorithm.ts
@@ -1,6 +1,15 @@
-import { O_TOKEN, ROWS, S_TOKEN, WORD_SIZE } from "./constants";
+import { O_TOKEN, S_TOKEN, WORD_SIZE } from "./constants";
 import { Cell, Word } from "./models";
 
+/**
+ * Checks if the given word is valid based on the following criteria:
+ * - The word must have the correct number of cells (WORD_SIZE).
+ * - The tokens must be in the correct order ('O', 'S', 'O').
+ * - The cells must be aligned in the same row, column, or diagonal.
+ * 
+ * @param {Word} word - An array of Cell objects representing the word.
+ * @returns {boolean} - Returns `true` if the word is valid, otherwise `false`.
+ */
 export function wordIsValid(word: Word): boolean {
 // Check if the selection has the correct number of cells
   if (word.length !== WORD_SIZE) {
@@ -72,6 +81,14 @@ for (let i = 1; i < word.length; i++) {
   return true;
 }
 
+/**
+ * A generator function that yields the current state of the word being formed.
+ * 
+ * Keeps track of the cells that have been clicked in order to form a word.
+ * It yields the current state of the word each time a new cell is added.
+ * 
+ * @yields {Word} - The current state of the word being formed.
+ */
 export function* wordMarker(): Generator<number, Word, Cell> {
   let clickedCells: Word = [];
   for (let i = 0; i < WORD_SIZE; i++) {
@@ -83,6 +100,15 @@ export function* wordMarker(): Generator<number, Word, Cell> {
   return clickedCells;
 }
 
+/**
+ * Compares two numbers and returns a value indicating their relative order.
+ * 
+ * This function is used as a comparator for sorting numbers in ascending order.
+ * 
+ * @param {number} a - The first number to compare.
+ * @param {number} b - The second number to compare.
+ * @returns {number} - Returns a negative value if `a` is less than `b`, zero if they are equal, and a positive value if `a` is greater than `b`.
+ */
 export function compareNumbers(a: number, b: number): number {
   return a - b;
 }
